6a79c5568d43eddbc08cc4e03bc49060bb507bd7
MudgalTech <122475855+MudgalTech@users.noreply.github.com>
2026-02-12 10:22:01 +0530

    Update Test123.java

--- Test123.java
+++ Test123.java
@@ -1,10 +1,14 @@
 public class Test123 {
 
     public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
-        int result = a * b;
 
-        System.out.println("This is For Multiplication Result: " + result);
+        int a = 15;
+        int b = 5;
+
+        int sum = a + b;
+        int multiply = a * b;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Multiplication: " + multiply);
     }
 }
