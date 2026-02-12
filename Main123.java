a8d2451f01d1e05e287e9d99f8edd3575030f643
MudgalTech <122475855+MudgalTech@users.noreply.github.com>
2026-02-12 09:56:07 +0530

    Add Main123 class with multiplication example

---
+++ Main123.java
@@ -0,0 +1,10 @@
+public class Main123 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int result = a * b;
+
+        System.out.println("Multiplication Result: " + result);
+    }
+}
